e3c630b95f7ba18e2cc6c62e62b0e0201d64e29a
Gabriel Aguirre <gaguirr2@andrew.cmu.edu>
2024-03-14 15:59:38 -0400

    n

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,8 +8,19 @@ export default function QueryProcessor(query: string): string {
   }
   if (query.toLowerCase().includes("what is your name")) {
     return (
-      "Gabriel Aguirre"
+      "GabrielA"
     );
+  }
+  if (query.toLowerCase().includes("what is")) {
+    let numbers = query.match(/\d+/g);
+    if (numbers && numbers.length >= 2) {
+      let num1 = parseInt(numbers[0]);
+      let num2 = parseInt(numbers[1]);
+      let result = num1 + num2;
+      return (
+        result + ""
+      );
+    }
   }
 
   return "";
